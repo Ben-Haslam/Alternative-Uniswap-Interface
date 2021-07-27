9d0331a22db1765a38555a713173cb0258d44346
Matthew French <matthewfrench019@gmail.com>
2021-07-27 17:50:04 +0100

    Added route for new currency component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import _App from "./ethereum";
 import Swap from "./Swap";
 import Liquidity from "./Liquidity";
 import NarBar from "./NavBar/NavBar";
+import CurrencySwapper from "./CurrencySwapper/CurrencySwapper"
 import { Route, Link } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <NarBar />
       <Route exact path="/uniswap-react/" component={Swap} />
+      <Route exact path="/uniswap-react/swap-beta" component={CurrencySwapper} />
       <Route exact path="/uniswap-react/liquidity" component={Liquidity} />
     </div>
   );
