a3402f244a11b29c0c74d26c074ef21b4f833950
Matthew French <matthewfrench019@gmail.com>
2021-07-27 17:48:06 +0100

    Added basic tokens

---
+++ src/constants/coins.js
@@ -0,0 +1,13 @@
+export const TOKEN_A = {
+    name: "Token A",
+    abbr: "TA",
+    address: "0x1d29BD2ACedBff15A59e946a4DE26d5257447727"
+}
+
+export const TOKEN_B = {
+    name: "Token B",
+    abbr: "TB",
+    address: "0xc108a13D00371520EbBeCc7DF5C8610C71F4FfbA"
+}
+
+export const ALL = [TOKEN_A, TOKEN_B];
