93f7a0640a5f89f28f923ad0b958b3883fb2b2cf
Ben-Haslam <ben.haslam@clearmatics.com>
2022-01-10 12:39:51 +0000

    add parastate

--- src/constants/coins.js
+++ src/constants/coins.js
@@ -65,15 +65,26 @@ const PARASTATECoins = [
     address: "", // Weth address is fetched from the router
   },
   {
-    name: "alice_erc20_token",
-    abbr: "ALI",
-    address: "0x264f76445895086E47AeCf7A663446AFB5397360",
+    name: "Token A",
+    abbr: "TA",
+    address: "0x4EDFE8706Cefab9DCd52630adFFd00E9b93FF116",
   },
   {
-    name: "bob_erc20_token",
-    abbr: "BOB",
-    address: "0xCC46783CE7444E2F7d606772A53139A9625103E8",
+    name: "Token B",
+    abbr: "TB",
+    address: "0x4489D87C8440B19f11d63FA2246f943F492F3F5F",
   },
+  
+  {
+    name: "Token C",
+    abbr: "TC",
+    address: "0x1d29BD2ACedBff15A59e946a4DE26d5257447727",
+  },
+  {
+    name: "Token D",
+    abbr: "TD",
+    address: "0xc108a13D00371520EbBeCc7DF5C8610C71F4FfbA",
+  }
 ]
 
 const MAINNETCoins = [
